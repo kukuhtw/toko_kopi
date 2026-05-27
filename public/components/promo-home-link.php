5a35031ae3070a6e67a6696e2dd73f532feeb77f
Kukuh TW <kukuhtw@gmail.com>
2026-05-28 01:15:38 +0700

    Add homepage promo link component

---
+++ public/components/promo-home-link.php
@@ -0,0 +1,7 @@
+<section class="promo-home-link">
+    <div class="promo-banner">
+        <h2>Promo & Penawaran Terbaru</h2>
+        <p>Lihat informasi promo, diskon, voucher, dan penawaran spesial terbaru dari toko kami.</p>
+        <a href="/promo/index.php" class="btn-promo">Lihat Semua Promo</a>
+    </div>
+</section>
